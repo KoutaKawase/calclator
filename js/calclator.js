db0846a56e7a9799c5a87af56ea3a71f6ae29bc8
KoutaKawase <suhrkawase@gmail.com>
2019-09-08 22:26:51 +0900

    小数点実装

--- js/calclator.js
+++ js/calclator.js
@@ -9,6 +9,8 @@ window.onload = function () {
     const operatorButtons = document.getElementsByClassName('operator');
     //ACボタン取得
     const allClearButonn = document.getElementById('all-clear');
+    //小数点ボタン取得
+    const decimalButton = document.getElementById('decimal');
     //押された数字と演算子格納用
     let choicedNumber;
     let choicedOperator;
@@ -44,4 +46,16 @@ window.onload = function () {
         screen.value = '0';
     }
 
+    //小数点ボタンが押されたらスクリーンに表示
+    decimalButton.onclick = function () {
+        //スクリーンの最後の文字を数値で取得
+        const screenLastWord = parseInt(screen.value.slice(-1));
+        //数字の後以外は小数点をつけられないようにする
+        if (isFinite(screenLastWord)) {
+            //スクリーン最後に小数点追加
+            screen.value += decimalButton.getAttribute('value');
+        } else {
+            alert('小数点を連続では打てません！');
+        }
+    }
 }
